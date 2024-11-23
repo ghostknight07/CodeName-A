9998abb1bae3a4cd1e8539339b9ba82837f2e02b
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-11-23 17:01:54 +0600

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import './Navbar.css';
 
-export default function Navbar(){
+export default function Navbar() {
   return (
     <header className="nav-header">
       <div>
         <h1 className='logo'>TIMEJUMP</h1>
       </div>
-      <div></div>
+      <div className='nav-links'>
+        <a href="#">Home</a>
+        <a href="#">Movies</a>
+        <a href="#">Blog</a>
+        <a href="#">Request</a>
+        <a href="#">About Us</a>
+      </div>
     </header>
   )
 }
