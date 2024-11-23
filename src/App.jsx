745f7032d5df83f80cdcdaa4b9fbd9e96a986510
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-11-23 16:39:48 +0600

    navbar component file added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <h1>This is the app</h1>
+    </main>
+  );
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+export default function Navbar(){
+  return (
+    <header className="nav-header">
+      <div>
+        <h1 className='logo'>TIMEJUMP</h1>
+      </div>
+      <div></div>
+    </header>
+  )
+}
