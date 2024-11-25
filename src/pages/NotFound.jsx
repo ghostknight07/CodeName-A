37716ddb083bac88c7e5dc37141e5d6145b422d1
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-11-25 10:36:43 +0600

    added some styles for not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,13 @@
+// css module file is an isolated file that has no
+// effect on other elements not even the classes or ids
+import styles from './NotFound.module.css';
+
 export default function NotFound(){
   return (
-    <>
-      <h1>404 Not Found</h1>
-      <a href="/">Go to home page</a>
-    </>
+    <div className={styles['notfound']}>
+      <h1 className={styles['not-found-main-text']}>404</h1>
+      <h1 className={styles['not-found-header-text']}>Page Not Found</h1>
+      <p className={styles['not-found-p']}>Take me back to the <a className={styles['not-found-a']} href='/'>Home</a>  page</p>
+    </div>
   )
 }
