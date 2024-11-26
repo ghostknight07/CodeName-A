53ae066ba3f601bc44e3239cc874ed9139bf25f7
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-11-26 12:14:12 +0600

    bug fixes on poster component

--- src/components/MoviePoster.jsx
+++ src/components/MoviePoster.jsx
@@ -1,47 +1,27 @@
-// instruction
-// you must read the docs of boxicons in
-// case you want to add more icons
-
 import 'boxicons/css/boxicons.min.css';
-
 import './MoviePoster.css';
-
 import PropTypes from "prop-types";
 
-// h- 250px
-// w- 150px
-
 export default function MoviePoster(props) {
-  const MoviePosterStyles = {
-    height: '250px',
-    width: '166px',
-    backgroundImage: `url(${props.posterUrl})`,
-    borderRadius: '16px', // Test with a solid color
-    backgroundSize: 'contain',
-    backgroundRepeat: 'no-repeat', // Prevents the image from repeating
-    backgroundPosition: 'center', // Centers the image in the div
-    cursor: 'pointer',
-  };
-
   return (
     <div className='movie-poster'>
 
-      <div id="movie-poster-img" style={MoviePosterStyles}>
+      <div id="movie-poster-img">
+        {/* Movie poster image */}
+        <img src={props.posterUrl} alt={`${props.movieName} poster`} />
 
         <p className='play-text'>
-          {/* an icon with boxicons */}
+          {/* Icon with boxicons */}
           <i className='bx bx-lg bx-play'></i>
         </p>
-
       </div>
 
-      {/* movie name */}
+      {/* Movie name */}
       <div><p>{props.movieName}</p></div>
 
     </div>
-  )
+  );
 }
-
 
 MoviePoster.propTypes = {
   posterUrl: PropTypes.string.isRequired,
