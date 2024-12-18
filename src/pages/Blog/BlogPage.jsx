6ce9b82369c89bbc30f45083123b1896d64b43aa
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-12-18 18:00:58 +0600

    added dynamic blog rendering and also a blogdata json file. and added some margin for the blog component

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -1,17 +1,48 @@
+// to do
+// add a link method or whatever it is called to each blog(in the loop)
+// add a prop for the blog image
+
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import Blog from "../../components/Blog/Blog.jsx";
+import { useState } from "react";
+
+// importing movie data json file
+import blogData from '../BlogData/BlogData.json';
 
 import './BlogPage.css'
 
 function BlogPage() {
+
+  const [visibleCount, setVisibleCount] = useState(10);
+
+  const handleClick = () => {
+    setVisibleCount((prevCount) => prevCount + 10);
+  };
+
+
   return (
     <div className="blog-container">
       <Navbar />
       <div className="blog-section">
         <div className="blogs pxy-10">
+          {blogData.slice(0, visibleCount).map((blog, index) => (
+            <Blog
+              key={index}
+              state={blog}
+              authorName={blog.authorName}
+              blogTitle={blog.blogTitle}
+              blogDescription={blog.blogDescription}
+              pubDate={blog.pubDate}
+            />
+          ))}
 
-          <Blog />
-
+          {visibleCount < blogData.length && (
+            <div className="button-container">
+              <button className="show-more-btn" onClick={handleClick}>
+                Show More
+              </button>
+            </div>
+          )}
         </div>
         <div className="populer-picks pxy-10">staff picks here</div>
       </div>
