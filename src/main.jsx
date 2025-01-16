e0956aec4317a631b13041043e08a5c2e7bb766d
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-01-16 18:30:07 +0600

    change movies to streaming in navbar

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: '/movies',
+    path: '/streaming',
     element: <MoviePage />,
     errorElement: <NotFound />,
   },
