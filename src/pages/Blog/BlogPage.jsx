36d5a1d27e96ea620a7448ab8da98f1e654ab3ea
ghostknight07 <himeltasin61@gmail.com>
2025-01-16 22:51:16 +0600

    add blogpage - pagenumber system

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -1,69 +1,86 @@
-// to do
-// add a link method or whatever it is called to each blog(in the loop)
-// add a prop for the blog image
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import Blog from "../../components/Blog/Blog.jsx";
 import PopularBlog from "../../components/PopularBlog/PopularBlog.jsx";
 import { useState } from "react";
 
-// importing movie data json file
-import blogData from '../BlogData/BlogData.json';
+// Importing blog data JSON file
+import blogData from "../BlogData/BlogData.json";
 
-import './BlogPage.css';
+import "./BlogPage.css";
 
 function BlogPage() {
-  const [visibleCount, setVisibleCount] = useState(10);
+  const itemsPerPage = 5; // Number of blogs per page
+  const totalPages = Math.ceil(blogData.length / itemsPerPage); // Total number of pages
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const handleClick = () => {
-    setVisibleCount((prevCount) => prevCount + 10);
+  // Get blogs for the current page
+  const currentBlogs = blogData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  // Handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
     <div className="blogPage-Main">
-      
       <div className="blogFullPage">
         <Navbar />
-
-      <div className="blogSection-container">
-
+        
+      <div className="blogResorcesContainer">
+        
+        <div className="blogSection-container">
+          {/* Normal Blogs Section */}
           <div className="NormalblogSection-container">
-           {blogData.slice(0, visibleCount).map((blog, index) => (
-             <Blog
-               key={index}
-               authorName={blog.authorName}
-               blogTitle={blog.blogTitle}
-               blogDescription={blog.blogDescription}
-               pubDate={blog.pubDate}
-               blogImage={blog.blogImage}
-             />
-           ))}
+            {currentBlogs.map((blog, index) => (
+              <Blog
+                key={index}
+                authorName={blog.authorName}
+                blogTitle={blog.blogTitle}
+                blogDescription={blog.blogDescription}
+                pubDate={blog.pubDate}
+                blogImage={blog.blogImage}
+              />
+            ))}
+          </div>
+      
 
-           {visibleCount < blogData.length && (
-             <div className="button-container">
-              <button className="show-more-btn" onClick={handleClick}>
-                 Show More
-               </button>
-             </div>
-           )}
-         </div>
+          {/* Popular Blogs Section */}
           <div className="populerSection-container">
-           {/* only show the component if ifPopular is true */}
-           {blogData.slice(0, visibleCount).map((blog, index) =>
-             blog.isPopular && (
-               <PopularBlog
-                 key={index}
-                 blogTitle={blog.blogTitle}
-                 blogImage={blog.blogImage}
-                 authorName={blog.authorName}
-               />
-             )
-           )}
-         </div>
-         </div>
-       </div>
-     </div>
- );
+            {currentBlogs.map(
+              (blog, index) =>
+                blog.isPopular && (
+                  <PopularBlog
+                    key={index}
+                    blogTitle={blog.blogTitle}
+                    blogImage={blog.blogImage}
+                    authorName={blog.authorName}
+                  />
+                )
+            )}
+          </div>
+          </div>
+
+          {/* Pagination */}
+          <div className="paginationContainer">
+            {[...Array(totalPages)].map((_, pageIndex) => (
+              <button
+                key={pageIndex}
+                className={`paginationButton ${
+                  currentPage === pageIndex + 1 ? "activePage" : ""
+                }`}
+                onClick={() => handlePageChange(pageIndex + 1)}
+              >
+                {pageIndex + 1}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BlogPage;
-
