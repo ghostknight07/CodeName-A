7b9ce6b00bb80837a64e77b1151dc3383669497b
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-01-23 10:33:38 +0600

    reformatted the full movie details page code.

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -21,61 +21,72 @@ function MovieDetails() {
 
   return (
     <div className={style.MovieDetailsMainPage}>
-          <img
-              className={style.GamesPageBackground}
-              src={movie.movieBackground}
-              alt="Background"
-            />
+      <img
+        className={style.GamesPageBackground}
+        src={movie.movieBackground}
+        alt="Background"
+      />
       <div className={style.MovieDetailsFullpage}>
         <Navbar />
         <div className={style.MovieDetailsResorcesContainer}>
           <div className={style.MovieDetailsContainer}>
-          
-
             <div className={style.MovieDetailsImageContainer}>
-            <img src={movie.posterUrl} alt={movie.movieName} />
-           </div>
-           
-          
-           <div className={style.MovieDetailsMovieName} >
-            <h1 className={style.MovieName}>{movie.movieName}</h1>
-           </div>
-           
-           <div className={style.MovieDetailsDRT}>
-            <p className={style.MovieDetailsDRT}>{movie.movieDRT}</p>
-           </div>
-            
-           <div className={style.MovieDetailsRatingContainer}>
-              <div>
-                <IconContext.Provider value={{ color: "yellow", size: "1.5em" }}>
-                  <FaImdb />
-                </IconContext.Provider>
+              <img src={movie.posterUrl} alt={movie.movieName} />
+            </div>
+
+            <div>
+              <div className={style.MovieDetailsMovieName}>
+                <h1 className={style.MovieName}>{movie.movieName}</h1>
               </div>
-              <div>
-                <IconContext.Provider value={{ color: "yellow", size: "1.5em" }}>
-                  <FaStar />
-                </IconContext.Provider></div>
-              
-              <div className={style.MovieDetailsRating}>
-                <p className={style.MovieDetailsRating}><span className={style.RatingSpan}>{movie.movieRating}</span>/10</p>
+
+              <div className={style.MovieDetailsDRT}>
+                <p className={style.MovieDetailsDRT}>{movie.movieDRT}</p>
+              </div>
+
+              <div className={style.MovieDetailsRatingContainer}>
+                <div>
+                  <IconContext.Provider value={{ color: "yellow", size: "1.5em" }}>
+                    <FaImdb />
+                  </IconContext.Provider>
+                </div>
+                <div>
+                  <IconContext.Provider value={{ color: "yellow", size: "1.5em" }}>
+                    <FaStar />
+                  </IconContext.Provider>
+                </div>
+                <div className={style.MovieDetailsRating}>
+                  <p className={style.MovieDetailsRating}>
+                    <span className={style.RatingSpan}>{movie.movieRating}</span>
+                    /10
+                  </p>
+                </div>
+              </div>
+
+              <div className={style.MovieDetailsDescriptionContainer}>
+                <p className={style.MovieDetailsDescription}>
+                  {movie.movieDescription}
+                </p>
+              </div>
+
+              <div className={style.MovieDetailsGenreContainer}>
+                <p className={style.MovieDetailsGenre}>
+                  <span>Genre(s):</span>
+                  {movie.movieGenre}
+                </p>
+              </div>
+
+              <div className={style.MovieDetailsWatchContainer}>
+                <a
+                  href={movie.movieLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className={style.MovieDetailsWatchButton}>
+                    Watch Now
+                  </button>
+                </a>
               </div>
             </div>
-
-            <div className={style.MovieDetailsDescriptionContainer}>
-              <p className={style.MovieDetailsDescription}>{movie.movieDescription}</p>
-            </div>
-
-            <div className={style.MovieDetailsGenreContainer}>
-            <p className={style.MovieDetailsGenre}><span>Genre(s):</span>{movie.movieGenre}</p>
-            </div>
-
-            <div className={style.MovieDetailsWatchContainer}>
-              <a href={movie.movieLink} target="_blank" rel="noopener noreferrer">
-              <button className={style.MovieDetailsWatchButton}>Watch Now</button> 
-              </a>
-            </div>
-
-
           </div>
         </div>
       </div>
