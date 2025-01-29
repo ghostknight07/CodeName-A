6e8a204f507530c18267d366fbbaf02287fcc745
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-01-29 17:17:49 +0600

    [style],[component]: rewritten navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import './Navbar.css';
 
 export default function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <header className="nav-header">
       <div className='nav-logo'>
         <a href="/">
-            <h1>CodeName A</h1>
+          <h1>CodeName A</h1>
         </a>
       </div>
       <div className='nav-links'>
         <a href="/">Home</a>
-        <a href="/streaming">Streaming</a>
-        <a href="/blog">Blog</a>
-        <a href="/games">Games</a>
-        <a href="/request">Request</a>
-        {/* Don't think an about page is needed. */}
-        {/* <a href="/about">About Us</a> */}
+
+        {/* Dropdown */}
+        <div className="dropdown">
+          <button className="dropbtn" onClick={() => setDropdownOpen(!dropdownOpen)}>
+            More ▼
+          </button>
+          <div className={`dropdown-content ${dropdownOpen ? "show" : ""}`}>
+            <a href="/streaming">Streaming</a>
+            <a href="/blog">Blog</a>
+            <a href="/games">Games</a>
+            <a href="/request">Request</a>
+          </div>
+        </div>
       </div>
     </header>
-  )
+  );
 }
