7b7bd84ce5407b02a613bf2a464c29d8703c7bb0
ghostknight07 <himeltasin61@gmail.com>
2025-01-31 17:03:09 +0600

    [style] : change navbar style. Dropdown is only for shows now

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react';
 import './Navbar.css';
 
 export default function Navbar() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [showsDropDown, setshowsDropDown] = useState(false);
-
   return (
     <header className="nav-header">
       <div className='nav-logo'>
@@ -14,26 +11,22 @@ export default function Navbar() {
       </div>
       <div className='nav-links'>
         <a href="/">Home</a>
+
+        
         <div className="dropdown">
-          <button className="dropbtn" onClick={() => setshowsDropDown(!showsDropDown)}>
+          <button className="dropbtn">
             Shows ▼
           </button>
-          <div className={`dropdown-content ${showsDropDown ? "show" : ""}`}>
+          <div className={`dropdown-content`}>
             <a href="/movies">Movies</a>
             <a href="/Tv Series">Tv Series</a>
           </div>
         </div>
         {/* Dropdown */}
-        <div className="dropdown">
-          <button className="dropbtn" onClick={() => setDropdownOpen(!dropdownOpen)}>
-            More ▼
-          </button>
-          <div className={`dropdown-content ${dropdownOpen ? "show" : ""}`}>
-            <a href="/blog">Blog</a>
-            <a href="/games">Games</a>
-            <a href="/request">Request</a>
-          </div>
-        </div>
+        
+        <a href="/blog">Blog</a>
+        <a href="/games">Games</a>
+        <a href="/request">Request</a>
       </div>
     </header>
   );
