631328a22559485c4a23facc3ebfcc33f036a4ea
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-06-11 17:49:37 +0600

    dont know what have changed

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,12 +1,16 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return(
-        <div className="w-full h-[70px]">
+        <div className="w-full h-[70px] border-b border-gray-600">
             <div className="w-[90%] sm:w-[80%] h-full mx-auto flex justify-between items-center">
                 <div>
+                    <Link href="/">
                     <h1 className="text-2xl font-bold">CodeName-A</h1>
+                    </Link>
                 </div>
                 <div>
-                    <ul className="flex gap-4">
+                    <ul className="flex gap-6">
                         <li>
                             <a href="#">Home</a>
                         </li>
